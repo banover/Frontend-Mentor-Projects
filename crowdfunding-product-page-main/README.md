b523bf029f3b3184d4be2a6e6cf675bbeed664da
banover <qkrwotn5713@naver.com>
2023-08-21 14:54:14 +0900

    후원에 따른 state 변화 기능 구현

--- crowdfunding-product-page-main/README.md
+++ crowdfunding-product-page-main/README.md
@@ -15,19 +15,11 @@
 removeAttribute를 통해 checked attribute를 제거한다.
 단순히 checked를 true/false하는 것으로는 의도한데로 작동하지 않는다.
 
-## The challenge
+-svg를 styling할 때,
+svg 구조를 자세히 보면 circle, path 등이 있다.
+css를 활용해서 수정할 수 있는 property들이 있으니 참고하자.
+또한 fill을 통해 색을 바꿀 때도, svg안에 있는 element를 선택해서 fill을
+적용해줘야 원하는 결과가 나온다.
 
-Your challenge is to build out this crowdfunding product page and get it looking as close to the design as possible.
-
-You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
-
-Your users should be able to:
-
-- View the optimal layout depending on their device's screen size
-- See hover states for interactive elements
-- Make a selection of which pledge to make
-- See an updated progress bar and total money raised based on their pledge total after confirming a pledge
-- See the number of total backers increment by one after confirming a pledge
-- Toggle whether or not the product is bookmarked
-
-Want some support on the challenge? [Join our Slack community](https://www.frontendmentor.io/slack) and ask questions in the **#help** channel.
+-form안의 input의 value의 흔적을 지우고 싶다면,
+value에 null 값을 부여하면 된다.
--- crowdfunding-product-page-main/index.js
+++ crowdfunding-product-page-main/index.js
@@ -1,3 +1,6 @@
+let TOTAL_BACKED_MOUNT = 89914;
+let TOTAL_BACKED_PERSON = 5007;
+
 const noRewardCard = document.querySelector(".card1");
 const noRewardCardRadioInput = document.querySelector(
   ".card1 input[type='radio']"
@@ -35,6 +38,40 @@ const completeModal = document.querySelector(
   ".Crowdfunding__completedModalCard"
 );
 const bookmarkBtn = document.querySelector(".Crowdfunding__bookmarkBtn");
+
+const bookmarkBtnElement = document.querySelector(
+  ".Crowdfunding__bookmarkBtn button"
+);
+const bookmarkImgBoxElement = document.querySelector(
+  ".Crowdfunding__bookmarkImgBox"
+);
+const bookmarkBackgroundImg = document.querySelector(
+  ".Crowdfunding__bookmarkImgBox svg circle"
+);
+const bookmarkFlagImg = document.querySelector(
+  ".Crowdfunding__bookmarkImgBox svg path"
+);
+const bookmarksvg = document.querySelector(".Crowdfunding__bookmarkImgBox svg");
+
+const allBackedInput = document.querySelectorAll(
+  ".Crowdfunding__modalHiddenCard input"
+);
+
+const allModalForm = document.querySelectorAll(
+  ".Crowdfunding__modalHiddenBtnBox form"
+);
+const supportingBackedMount = document.querySelector(
+  ".Crowdfunding__supportingMoney"
+);
+const totalBackedPerson = document.querySelector(
+  ".Crowdfunding__supportingBackers"
+);
+
+const targetBarElement = document.querySelector(
+  ".Crowdfunding__targetBarBox progress"
+);
+
+targetBarElement.value = (TOTAL_BACKED_MOUNT / 100000) * 100;
 
 noRewardCard.addEventListener("click", () => {
   BlackEditionCard.style.border = "1.8px solid rgb(0, 0, 0, 0.1)";
@@ -87,6 +124,7 @@ openModalBtn.addEventListener("click", () => {
   BambooCardRadioInput.removeAttribute("checked");
   noRewardCardRadioInput.removeAttribute("checked");
   BlackEditionCardRadioInput.removeAttribute("checked");
+  allBackedInput.forEach((input) => (input.value = null));
   hiddenBambooCard.style.display = "none";
   hiddenBlackEditionCard.style.display = "none";
   hiddenNoRewardCard.style.display = "none";
@@ -94,32 +132,44 @@ openModalBtn.addEventListener("click", () => {
   modal.style.display = "block";
 });
 
-// 이벤트 위임으로 코드수정할 수 있다.
-allModalContinueBtn.forEach((btn) =>
-  btn.addEventListener("click", () => {
+allModalForm.forEach((form) =>
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+
+    const inputBackedMount = e.target[0].value
+      ? e.target[0].value
+      : e.target[0].placeholder;
+    console.log(inputBackedMount);
+
+    TOTAL_BACKED_MOUNT += inputBackedMount ? Number(inputBackedMount) : 0;
+    TOTAL_BACKED_PERSON++;
+    console.log(TOTAL_BACKED_MOUNT);
+    supportingBackedMount.textContent = `$${TOTAL_BACKED_MOUNT.toLocaleString(
+      "en-US"
+    )}`;
+    totalBackedPerson.textContent = `${TOTAL_BACKED_PERSON.toLocaleString(
+      "en-US"
+    )}`;
+    targetBarElement.value = (TOTAL_BACKED_MOUNT / 100000) * 100;
+
     modal.style.display = "none";
     completeModal.style.display = "flex";
   })
 );
 
+// 이벤트 위임으로 코드수정할 수 있다.
+// allModalContinueBtn.forEach((btn) =>
+//   btn.addEventListener("click", () => {
+//     modal.style.display = "none";
+//     completeModal.style.display = "flex";
+//   })
+// );
+
 completeModalCheckBtn.addEventListener("click", () => {
   modalBackground.style.display = "none";
   completeModal.style.display = "none";
 });
-
-const bookmarkBtnElement = document.querySelector(
-  ".Crowdfunding__bookmarkBtn button"
-);
-const bookmarkImgBoxElement = document.querySelector(
-  ".Crowdfunding__bookmarkImgBox"
-);
-const bookmarkBackgroundImg = document.querySelector(
-  ".Crowdfunding__bookmarkImgBox svg circle"
-);
-const bookmarkFlagImg = document.querySelector(
-  ".Crowdfunding__bookmarkImgBox svg path"
-);
-const bookmarksvg = document.querySelector(".Crowdfunding__bookmarkImgBox svg");
 
 bookmarkBtn.addEventListener("click", () => {
   bookmarkBtnElement.textContent =
